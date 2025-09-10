c4aa42a9afafb92b645f50c87dc5f402c13c451a
D3adSec <bruno.emedeiros99@gmail.com>
2025-09-10 11:47:41 -0300

    aula 10/09

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { listaProdutos } from "./produtos.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let listaPessoas = [
-  { id: 1, nome: "Bruno", idade: 26 },
-  { id: 2, nome: "Bruno", idade: 16 },
-  { id: 3, nome: "Arthur", idade: 16 },
-  { id: 4, nome: "Arthur", idade: 16 },
-];
-
-app.get("/perfil/:id", (requisicao, resposta) => {
-  const { id } = requisicao.params;
-
-  const pessoa = listaPessoas.find((pessoa) => pessoa.id == id);
-
-  return resposta.status(200).json(pessoa);
+app.get("/produtos", (requisicao, resposta) => {
+  return resposta.status(200).json(listaProdutos);
 });
 
 app.listen(3000, () => {
--- front-end/script.js
+++ front-end/script.js
@@ -1,7 +1,24 @@
-const botao = document.querySelector("#botao");
+window.addEventListener("load", async () => {
+  const div_root = document.querySelector("#root");
+  const resposta = await fetch("http://192.168.1.15:3000/produtos");
+  const produtos = await resposta.json();
 
-botao.addEventListener("click", async () => {
-  const resposta = await fetch("http://192.168.1.15:3000/perfil/1");
-  const dados = await resposta.json();
-  console.log(dados);
+  produtos.map((p) => {
+    const div_card = document.createElement("div");
+    div_card.id = "card";
+
+    const imagem_card = document.createElement("img");
+    imagem_card.src = p.imagem;
+
+    const titulo_card = document.createElement("h5");
+    titulo_card.innerText = p.titulo;
+
+    const preco_card = document.createElement("h6");
+    preco_card.innerText = "R$ " + p.preco;
+
+    div_card.appendChild(imagem_card);
+    div_card.appendChild(titulo_card);
+    div_card.appendChild(preco_card);
+    div_root.appendChild(div_card);
+  });
 });
